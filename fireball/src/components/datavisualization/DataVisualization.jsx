9c3e4358499779f7895d1467ff708e0e9b1d4e6d
ConwayCJ <conwaycj@plu.edu>
2023-08-10 19:55:17 -0700

    scaffold boilerplate application

---
+++ fireball/src/components/datavisualization/DataVisualization.jsx
@@ -0,0 +1,6 @@
+
+export default function DataVisualization() {
+  return (
+    <div>DataVisualization</div>
+  )
+}
---
+++ fireball/src/components/table/Table.jsx
@@ -0,0 +1,5 @@
+export default function Table() {
+  return (
+    <div>Table</div>
+  )
+}
