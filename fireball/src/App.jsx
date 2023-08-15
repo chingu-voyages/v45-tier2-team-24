d032a8cf5d44775706308e6cb1ef31c18c051732
Yodae <yodae.lim@gmail.com>
2023-08-15 16:30:12 -0700

    set up leaflet slippy map. plot dummy data

--- fireball/src/App.jsx
+++ fireball/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import Map from './components/map/Map'
 
 function App() {
 
 
   return (
     <>
-
+      <Map/>
     </>
   )
 }
--- fireball/src/components/map/Map.jsx
+++ fireball/src/components/map/Map.jsx
@@ -1,5 +1,32 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import data from './data';
+
 export default function Map() {
+  const myData = data.filter((element)=> element.reclat && element.reclong);
+  console.log(myData);
   return (
-    <div>Map</div>
-  )
+    <>
+      <h1>My map</h1>
+      <MapContainer
+        style={{ height: "450px", width: "100%" }}
+        center={[51.505, -0.09]}
+        zoom={1}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {myData.map(item =>(
+          <Marker key={item.id} position={[item.reclat, item.reclong]}>
+            <Popup>
+              {item.name}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
+  );
 }
