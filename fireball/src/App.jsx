cc941371941d284d6ed3d876f9eef11eeb6c50d1
carlos luevano <clue@pop-os.localdomain>
2023-08-16 21:27:29 -0500

    table component and api

--- fireball/src/App.jsx
+++ fireball/src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-
+import DataTable from './components/table/Table'
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null)
 
-
+  useEffect(() => {
+    const apiUrl = "https://data.nasa.gov/resource/gh4g-9sfh.json"
+    axios.get(apiUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data: ", error);
+      });
+  }, []);
+  
   return (
     <>
-
+    {data==null? <p>Loading...</p> : <DataTable  data={data}/>}
+    
     </>
   )
 }
