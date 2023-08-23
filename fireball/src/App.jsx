175ef7cd94f34ad1b4f443d47a577d10d07d8edd
carlos luevano <clue@pop-os.localdomain>
2023-08-22 20:53:59 -0500

    mobile redesign of table done

--- fireball/src/App.jsx
+++ fireball/src/App.jsx
@@ -5,21 +5,30 @@ import axios from 'axios';
 
 function App() {
   const [data, setData] = useState(null)
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const [sortedData, setSortedData] = useState(null)
 
   useEffect(() => {
     const apiUrl = "https://data.nasa.gov/resource/gh4g-9sfh.json"
     axios.get(apiUrl)
       .then((response) => {
         setData(response.data);
+        const sortedData = response.data.sort((a, b) => new Date(b.year) - new Date(a.year)).slice(0, 3);
+        setSortedData(sortedData);
       })
       .catch((error) => {
         console.error("There was an error fetching the data: ", error);
       });
+      
   }, []);
   
   return (
     <>
-    {data==null? <p>Loading...</p> : <DataTable  data={data}/>}
+    {sortedData == null? <p>Loading...</p> : <div className="p-4">
+      {sortedData.map((item, index) => (
+        <DataTable key={index} data={item} isOpen={openDropdown} setIsOpen={setOpenDropdown} />
+      ))}
+    </div>}
     
     </>
   )
--- fireball/src/components/table/Table.jsx
+++ fireball/src/components/table/Table.jsx
@@ -1,41 +1,26 @@
+import { useState } from 'react';
 
-
-function DataTable({data, cellWidth = "20", cellHight = "20", tableWidth = "full", textSize = "20px"}) {
-
-  console.log(data)
+function DataTable({data, isOpen, setIsOpen}) {
 
   return (
-    <div className={`w-full overflow-x-auto overflow-y-auto text-[${textSize}]`}>
-      <table className={`w-${tableWidth} border-collapse`}>
-        <thead>
-          <tr>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Name</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>ID</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Name Type</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Rec Class</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Mass</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Fall</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Year</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Latitude</th>
-            <th className={`tableHeader w-${cellWidth} h-${cellHight}`}>Longitude</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td className={`tableData w-${cellWidth} h-${cellHight}`}>{item.name}</td>
-              <td className="tableData">{item.id}</td>
-              <td className="tableData">{item.nametype}</td>
-              <td className="tableData">{item.recclass}</td>
-              <td className="tableData">{item.mass}</td>
-              <td className="tableData">{item.fall}</td>
-              <td className="tableData">{item.year?.slice(0,10)}</td>
-              <td className="tableData">{item.reclat}</td>
-              <td className="tableData">{item.reclong}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="w-full mb-1">
+      <button
+        onClick={() => setIsOpen(isOpen === data.id ? null : data.id)}
+        className="w-full bg-slate-700 text-white p-2"
+      >
+        {data.name} - Mass: {data.mass} kg
+      </button>
+      {isOpen === data.id && (
+        <div className="w-full bg-gray-100 p-4">
+          <p><strong>ID:</strong> {data.id}</p>
+          <p><strong>Name Type:</strong> {data.nametype}</p>
+          <p><strong>Mass:</strong> {data.mass} kg</p>
+          <p><strong>Fall:</strong> {data.fall}</p>
+          <p><strong>Date:</strong> {data.year?.slice(0,10)}</p>
+          <p><strong>Latitude:</strong> {data.reclat}</p>
+          <p><strong>Longitude:</strong> {data.reclong}</p>
+        </div>
+      )}
     </div>
   );
 }
