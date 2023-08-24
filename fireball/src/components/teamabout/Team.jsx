d194dbab612bd932f072fe88968c9cf1784852dc
RKVanHorn <acinonyx79@hotmail.com>
2023-08-23 22:01:35 -0700

    added meteor background

--- fireball/src/components/teamabout/Team.jsx
+++ fireball/src/components/teamabout/Team.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Team() {
-  return <div>Team</div>;
+  return (
+    <>
+      <div className="star"></div>
+      <div className="meteor-1"></div>
+      <div className="meteor-2"></div>
+      <div className="meteor-5"></div>
+      <div className="meteor-10"></div>
+    </>
+  );
 }
