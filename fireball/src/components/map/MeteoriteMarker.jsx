edc83091e7d701a61046ad54aca2b3bbf3913740
Yodae <yodae.lim@gmail.com>
2023-09-07 14:46:42 -0700

    change meteorite marker size and color during mouseover

--- fireball/src/components/map/MeteoriteMarker.jsx
+++ fireball/src/components/map/MeteoriteMarker.jsx
@@ -20,10 +20,22 @@ export default function MeteoriteMarker({
       opacity={0.5}
       radius={10000 * Math.log(parseInt(mass))}
       eventHandlers={{
-      click: (e) => {
-        updateSelectedMeteorite(id);
+        click: (e) => {
+          updateSelectedMeteorite(id);
+        },
+        mouseover: (e) => {
+          e.target.setRadius(e.target._mRadius * 1.1);
+          e.target.setStyle({ color: "#6633cc" });
+          //purple: #6633cc
+          //orange: #ff6600
+          //green: #00ff00
+        },
+
+        mouseout: (e) => {
+          e.target.setRadius(e.target._mRadius / 1.1);
+          e.target.setStyle({ color: '#3388ff' });// our original color
+        },
       }}
-    }
     >
       {/*
       <Popup>
