5e0bd1750afa518e5a96f6c3e65a1b2bc8af8bf7
RKVanHorn <acinonyx79@hotmail.com>
2023-09-14 10:07:40 -0700

    added test for team

--- fireball/src/__test__/app.test.jsx
+++ fireball/src/__test__/app.test.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import App from "../App";
 import Hero from "../components/hero/Hero";
+import Team from "../components/teamabout/Team";
 //import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react"; // (or /dom, /vue, ...)
-import {expect, test} from "vitest"
+import { expect, test } from "vitest";
 //app loader container
 it("should show meteorite loading screen", () => {
-  render(<App />)
+  render(<App />);
   const loadingScreen = screen.queryByText("/Meteortie Landing.../i");
   expect(loadingScreen).toBeDefined();
 });
@@ -14,7 +15,10 @@ it("should show meteorite loading screen", () => {
 //Hero
 
 //Team
-
+it("should render 4 astronaut image elements", () => {
+  render(<Team />);
+  expect(screen.getAllByRole("img")).to.have.lengthOf(4);
+});
 //Data visualization
 
 //Map
